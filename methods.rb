d88f5c0cfc3b339272373684f58302ef8660307a
Gabriella Elizondo <gabriellaelizondo@gmail.com>
2023-01-31 09:35:15 -0500

    commit tree f6e9a363b1d47d89593790e36f0c4d0973e2d519

--- methods.rb
+++ methods.rb
@@ -1,1 +1,23 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def great(name)
+    puts "Hello, #{name}!"
+end
+
+def greet_with_defualt(name="programmer")
+    puts "Hello, #{name}! "
+end
+
+def add(num1, num2)
+    return num1 + num2
+end
+
+def halve(num)
+    return nil unless num.class == Integer
+
+    num / 2
+end
+
+    
